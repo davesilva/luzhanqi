1d20b24f043a6b329b891584d081d23db45605f9
Nathan D. Holmes <holmes9@blandia.ccs.neu.edu>
2014-02-10 15:55:14 -0500

    initial recieve function in io.py

--- app/io.py
+++ app/io.py
@@ -5,8 +5,13 @@ import message
 
 
 def send(msg):
-    
      serial = msg.serialize()
      sys.stdout.write(serial)
      sys.stdout.flush()
 
+def recieve():
+     packet = sys.stdin.readlines()
+     msg = Message(packet)
+     return msg
+
+     
