66d517064d8ca21ac5bc61d29dfc1279fe11b7db
David Silva <dsilva917@gmail.com>
2014-02-10 17:50:25 -0500

    Add some documentation to the config module and ensure the return types are correct

--- app/config.py
+++ app/config.py
@@ -10,6 +10,8 @@ parser.add_option("-g", "--go", dest="turn", help="specify which player should g
 parser.add_option("-t", "--time/move", dest="time", help="specify the time per move")
 
 class Config:
+    # -> Config
+    # Create a new Config module, parsing command line arguments
     def __init__(self):
         (options, args) = parser.parse_args()
 
@@ -19,11 +21,19 @@ class Config:
             sys.stderr.write(parser.format_help())
             quit()
 
-        self.turn = options.turn
-        self.time = match.group(1)
+        if (re.search("ms", options.time)):
+            self.time = int(match.group(1))
+        else:
+            self.time = int(1000 * float(match.group(1)))
 
+        self.turn = int(options.turn)
+
+    # -> Integer
+    # Get this player's turn (1 if the player should go first, 2 if second)
     def get_turn(self):
         return self.turn
 
+    # -> Integer
+    # Get the time per move in milliseconds
     def get_time(self):
         return self.time
