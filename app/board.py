369def0a16ec8f5f28f0f8febf00593a87a7d69a
Mimi Lin <hloople@gmail.com>
2014-02-10 18:18:30 -0500

    New board and piece classes

---
+++ app/board.py
@@ -0,0 +1,59 @@
+# A Position is a tuple of Numbers (row, column)
+
+import copy 
+
+class Board:
+	def __init__(self):
+		self.pieces_list = []
+
+	def __init__(self, pieces_list):
+		self.pieces_list = pieces_list
+
+	# -> Board
+	def get_empty_board():
+		return Board()
+
+	# Piece Position -> Board
+	# Add piece to the board for initial setup
+	def place_piece(self, piece):
+		new_list = copy.deepcopy(self.pieces_list)
+		new_list.add(piece)
+		Board(new_list)
+
+	# Position Position -> Board
+	def move_piece(self, piece, dest)	
+		# List without piece
+		new_list = []
+		for p in self.pieces_list
+			if(!p.equals(piece))
+				ins_p = copy.deepcopy(p)
+				new_list.append(ins_p)
+		# Add list to piece
+		new_list.append(piece.move(dest))
+
+
+class Piece:
+
+	# Number Position -> Piece
+	def __init__(self, rank, position):
+		# Integer from 1 - 12
+		self.rank = rank
+		# Number tuple (row, column)
+		self.position = 0
+
+	# Position -> Piece
+	def move(self, new_posn):
+		return Position(self.rank, new_posn) 
+
+	# Piece -> Boolean
+	# Check if the given piece is the same as this instance
+	def equal(self, piece):
+		return (self.rank == piece.rank & self.tuple == pice.tuple)
+
+
+
+
+
+
+
+
