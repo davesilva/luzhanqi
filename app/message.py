5e07e5d5664528232d4134fa74af5735898e02da
Mimi Lin <hloople@gmail.com>
2014-03-14 10:32:02 -0400

    added receive message classes

--- app/message.py
+++ app/message.py
@@ -1,8 +1,18 @@
 import app.board as board
 
+INVALID_SETUP_REGEX = "(Invalid Board Move)"
+IM_PIECE_MOVE = "Piece not movable"
+IM_NO_PIECE = "No Piece"
+IM_FT_INVALID = "From To Invalid"
+IM_LOC_INVALID = "Location Invalid"
+INVALID_MOVE_REGEX = "(Invalid Board Move)( )(%s|%s|%s|%s)" % (IM_PIECE_MOVE, IM_NO_PIECE, IM_FT_INVALID, IM_LOC_INVALID)
+
+
+
 
 # Message super class
-class Message(object):
+class SendMessage(object):
+
     def __init__(self):
         pass
     def __str__(self):
@@ -11,7 +21,9 @@ class Message(object):
         pass
 
 
-class InitMessage(Message):
+
+
+class InitMessage(SendMessage):
     # -> Message
     # takes a Board, initializes the instance var
     def __init__(self, aboard):
@@ -22,7 +34,7 @@ class InitMessage(Message):
     def serialize(self):
         return self.board.serialize()
 
-class MoveMessage(Message):
+class MoveMessage(SendMessage):
     # -> Message
     # takes two tuples, and initializes the instance vars
     def __init__(self, posfrom, posto):
@@ -36,3 +48,31 @@ class MoveMessage(Message):
         t = self.posto
         base_char = ord('A')
         return "( %c%d %c%d )"%(base_char + p[0], p[1] + 1, base_char + t[0], t[1] + 1)
+
+
+
+class ReceiveMessage(object):
+
+    def __init__(self, msgstring):
+        self.msgstring = msgstring
+
+    def __str__(self):
+        pass
+
+    def deserialize(self):
+        rawstring = self.msgstring;
+        if(rawstring == ""):
+            try()
+
+
+
+
+class Error(Message):
+    # -> Message
+    def __init__(self, error):
+        self.error = error
+
+
+
+
+
