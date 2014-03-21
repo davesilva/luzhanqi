617a996d4e8691a35c2823a74bd6e79d025e543a
Mimi Lin <hloople@gmail.com>
2014-03-21 13:13:39 -0400

    added new position namedtuple

---
+++ app/position.py
@@ -0,0 +1,19 @@
+"""
+A Position is a NamedTuple Position(row, col) where
+row is 0 - 11 and col is 0 - 4. 
+
+"""
+import collections
+
+Position = collections.namedtuple('Position', ['row', 'col'])
+
+# Position -> String
+def pos_to_str(pos):
+    return chr(pos[0] + ord('A')) + str((pos[1] + 1))
+
+# String -> Position 
+def str_to_pos(s):
+	return Position(ord(s[0]) - ord('A'), int(s[1:]) - 1)
+
+
+print(str_to_pos("A1"))
