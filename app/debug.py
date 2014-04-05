3cf9f74adbd807a9917cdba43ce55a34225381de
David Silva <dsilva917@gmail.com>
2014-04-04 20:02:15 -0400

    Change the probability sum assertion to a debug statement

--- app/debug.py
+++ app/debug.py
@@ -1,4 +1,5 @@
-from sys import stderr
+import logging
+log = logging.getLogger("probability")
 
 
 def assert_all_probabilities_sum_to_one(board):
@@ -25,6 +26,4 @@ def assert_piece_probabilities_sum_to_one(piece):
 
     prob_sum = sum(piece.probability(rank) for rank in piece.ranks())
     if not prob_sum == 1:
-        print("Probability sum for %s is %s" %
-              (str(piece), str(prob_sum)), file=stderr)
-        assert(False)
+        log.info("Probability sum for %s is %s" % (str(piece), str(prob_sum)))
