810a121b0ecdbb21d23961df22faa9be00b0aac1
Mimi Lin <hloople@gmail.com>
2014-04-06 20:45:31 -0400

    added strategies that uses probability

--- app/strategy.py
+++ app/strategy.py
@@ -8,6 +8,8 @@ RANK_WORTH = {1:2, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 'B':5}
 RANK_INIT_AMT = {1:3, 2:3, 3:3, 4:2, 5:2, 6:2, 7:2, 8:1, 9:1, 'B':2}
 
 # Factors should all up to 1
+# TODO Dictionary: weight to functions 
+# TODO Make 2 dictionaries (player 1 and player 2)
 WORTH_FACTOR = 0.3
 LOSING_PIECE_FACTOR = 0.3
 COMMONALITY_FACTOR = 0.05
@@ -23,9 +25,10 @@ def action_value(board, src, dest):
 	Board Position Position -> Number
 	Returns the value of an action 
 	"""
-	return \
+	(win, loss, tie) = prob_win_loss_tie(board, src, dest)
+	value = \
 	WORTH_FACTOR * piece_worth(board, src) + \
-	LOSING_PIECE_FACTOR * prob_losing(board, src, dest) + \
+	LOSING_PIECE_FACTOR * (loss + tie) + \
 	COMMONALITY_FACTOR * piece_commonality_rating(board, src) + \
 	PROXIMITY_FACTOR * proximity_rating(board, src, dest) + \
 	BRACE_FACTOR * brave_rating() 
@@ -43,9 +46,17 @@ def piece_worth(board, pos):
 	rank = get_rank(p)
 	return RANK_WORTH[rank] / (max - min)
 
-def prob_losing(board, src, dest):
-	# TODO 
-	return 0.5
+def prob_win_loss_tie(board, src, dest):
+	"""
+	Board Position Position -> (Number, Number, Number)
+
+	Returns probability of winning, losing and tying
+
+	"""
+	player_piece = board.piece_at(src)
+	opponent_piece = board.piece_at(dest)
+	return player_piece.expected_attack_outcome(opponent_piece)
+
 
 def piece_commonality_rating(board, src):
 	"""
